c3dad1066c0b23a9f5cfc0cc169974edaa6a766a
Aviad <aviad339833@gmail.com>
2018-06-28 17:10:23 -0400

    complited the large-hero for all screens

--- app/gulp/tasks/styles.js
+++ app/gulp/tasks/styles.js
@@ -4,10 +4,11 @@ var autoprefixer = require('autoprefixer');
 var simpleVars = require('postcss-simple-vars');
 var nestCss = require('postcss-nested');
 var postcssImport = require('postcss-import');
+var mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([postcssImport, simpleVars, nestCss, autoprefixer]))
+		.pipe(postcss([postcssImport, mixins, simpleVars, nestCss, autoprefixer]))
 		.on('error', function(errorInfo){
 			console.log(errorInfo.toString())
 			this.emit('end');
