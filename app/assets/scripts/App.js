33519aec8c0d441dcdb3be655c07ba5de2d55668
Aviad <aviad339833@gmail.com>
2018-07-03 14:17:06 -0400

    finished tuched ups

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import	$ from 'jquery';
 
 new RevealOnScroll($('.feture-item'),'85%');
 new RevealOnScroll($('.service'), '60%');
+var stickyHeader = new StickyHeader();
